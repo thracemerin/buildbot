a29864da76388f916ec8ae4f4160bd59516290df
Trevor Gill <trevorjgill@gmail.com>
2013-01-12 19:36:36 -0500

    Added check for 1 clean per 24 hours.

--- build.sh
+++ build.sh
@@ -21,7 +21,21 @@ repo sync
 
 . vendor/cm/get-prebuilts
 
-make clean
+LAST_CLEAN=0
+if [ -f .clean ]
+then
+  LAST_CLEAN=$(date -r .clean +%s)
+fi
+TIME_SINCE_LAST_CLEAN=$(expr $(date +%s) - $LAST_CLEAN)
+TIME_SINCE_LAST_CLEAN=$(expr $TIME_SINCE_LAST_CLEAN / 60 / 60)
+if [ $TIME_SINCE_LAST_CLEAN -ge "24" ]
+then
+  echo "Cleaning!" 
+  touch .clean
+  make clobber
+else
+  echo "Last clean was: $TIME_SINCE_LAST_CLEAN hours ago, skipping clean"
+fi
 
 . build/envsetup.sh
 lunch cm_${LUNCH}-userdebug
